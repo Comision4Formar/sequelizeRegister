4bc04d96e9ae72e74d380b03615b51826eebaf50
EricFormar <66625345+EricFormar@users.noreply.github.com>
2021-02-26 13:28:53 -0300

    validacion register ok

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,9 +1,21 @@
+const {validationResult} = require('express-validator')
+
+
 module.exports = {
     register : (req,res) => {
         res.render('register')
     },
     processRegister : (req,res) => {
-        res.send(req.body)
+        let errores = validationResult(req);
+
+        if(errores.isEmpty()){
+            res.send(req.body)
+        }else{
+            return res.render('register',{
+                errores : errores.mapped(),
+                old: req.body
+            })
+        }
     },
     login : (req,res) => {
         res.render('login')
--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,12 @@
 var express = require('express');
 var router = express.Router();
 
-const {register,processRegister,login,processLogin,logout,profile} = require('../controllers/usersController') 
+const {register,processRegister,login,processLogin,logout,profile} = require('../controllers/usersController');
+
+const registerValidator = require('../validations/registerValidator');
 
 router.get('/register',register);
-router.post('/register',processRegister);
+router.post('/register',registerValidator, processRegister);
 
 router.get('/login',login);
 router.post('/login',processLogin);
